32720aaa2448da9bc33a9acce21439141f61baca
Renan Sgubin <78612307+RenanSgubin@users.noreply.github.com>
2022-05-27 23:38:19 -0300

    Add files via upload
    -Fixado bug quando o Knight usava a poção de vida
    -Adicionado botão de passar turno

--- js/battle.js
+++ js/battle.js
@@ -12,8 +12,15 @@ let charDef = 25;
 //Vida do personagem
 let charLife = 100;
 
+//Auxiliadora heal
+let charLifeHealAux = 0;
+let charLifeHealAuxTotal = 100;
+
 //Controlar a vez de cada personagem
 let charTurn = true;
+
+//Controlar cura
+let charHeal = 0;
 
 let skills = 
     [                  
@@ -32,7 +39,6 @@ let bottomPosition = 50;
 //Status do inimigo
 
 let charLifePercent = 0;
-let enemyDamage = 0;
 
 //Mana do Inimigo
 let enemyMana = 100;
@@ -42,6 +48,8 @@ let enemyDef = 15;
 
 //Vida do personagem
 let enemyLife = 100;
+
+let enemyDamage = 0;
 
 let enemySkills = 
     [                  
@@ -58,7 +66,6 @@ let enemySkills =
 //Numero que vai decidir os ataques do inimigo
 let enemySkillNumber = 0;
 
-
 function useAttack( skillNumber ) {
     
     if(charTurn) { //Caso seja vez do jogador jogar
@@ -70,8 +77,8 @@ function useAttack( skillNumber ) {
         charTurn = false; 
 
         //Desabilitar botões
-        for(let i = 0; i < 6; i++) {
-            document.getElementsByClassName("skill")[i].disabled = true;
+        for(let i = 0; i < 7; i++) {
+            document.getElementsByTagName("button")[i].disabled = true;
         }
         
         //Tirar mana do personagem 
@@ -87,9 +94,15 @@ function useAttack( skillNumber ) {
         charDef = charDef + ((charDef / 100) * skills[skillNumber][3]);
 
         //Curar
-        charLife = charLife + skills[skillNumber][4];
-        document.getElementsByClassName("life-width")[0].style.width = charLife+"%";
-        document.getElementsByClassName("life-number")[0].innerHTML = charLife;
+        charHeal = skills[skillNumber][4];
+        if(skillNumber === 2) {
+            if(charLifeHealAuxTotal > 100) {
+                alert("asasa");
+            }
+            document.getElementsByClassName("life-width")[0].style.width = charLifeHealAuxTotal+30+"%";
+            document.getElementsByClassName("life-number")[0].innerHTML = charLifeHealAuxTotal+30;
+        }
+        
         
 
         //Ataque
@@ -97,7 +110,8 @@ function useAttack( skillNumber ) {
             //Tirar vida do inimigo
             damage = damage + skills[skillNumber][1];
             //Porcentagem da vida do inimigo
-            enemyLifePercent = 100 - damage;
+            enemyLifePercent = 100 - (damage - (damage / 100 * 15));
+
             document.getElementsByClassName("life-width")[1].style.width = enemyLifePercent+"%";
             document.getElementsByClassName("life-number")[1].innerHTML = enemyLifePercent;
 
@@ -136,94 +150,134 @@ function useAttack( skillNumber ) {
         }
          
     }if(charTurn === false){ //Vez do inimigo
-        document.getElementsByClassName("pixel-right-arrow")[0].style.display = "block";
-        document.getElementsByClassName("pixel-left-arrow")[0].style.display = "none";
-
-         //Adicionar 10 de mana
-         enemyMana+= 10;
-         if(enemyMana > 100){
-             enemyMana = 100;
-         }
-         document.getElementsByClassName("mana-width")[1].style.width = enemyMana+"%";
-         document.getElementsByClassName("mana-number")[1].innerHTML = enemyMana;
-        
-
-        function enemyAttacks() {
-
-            //Faz um calculo com a mana pra sortear uma skill que esteja dentro do limite da mana
-            enemySkillNumber = (enemyMana / 10) / 2;
-                
-            if(enemySkills[Math.ceil(enemySkillNumber)][0] > enemyMana) {
-                //Conta vai dar 6, que é a skill referente a mana zerada
-                enemySkillNumber = 120;
-            }else {
-                //Tirar mana
-                enemyMana = enemyMana - enemySkills[Math.ceil(enemySkillNumber)][0];
-
-                //Tirar vida do Char
-                enemyDamage = enemyDamage + enemySkills[Math.ceil(enemySkillNumber)][1];
-                //Porcentagem da vida do Char
-                charLifePercent = 100 - enemyDamage;
-                charLife = charLifePercent;
-                console.log(charLife);
-                document.getElementsByClassName("life-width")[0].style.width = charLife+"%";
-                document.getElementsByClassName("life-number")[0].innerHTML = charLife;
-
-                //Aparecer dano no inimigo
-                document.getElementsByClassName("char-damage")[0].innerHTML = "-" + 
-                enemySkills[Math.ceil(enemySkillNumber)][1];
-
-                //Mover ele até sumir
-                function showEnemyDamage(){
-                    bottomPosition = bottomPosition + 0.1;
-                    document.getElementsByClassName("char-damage")[0].style.bottom = bottomPosition + "%";
-
-                    if(Math.round(bottomPosition) === 68) {
-                        document.getElementsByClassName("char-damage")[0].style.display = "none";
-                    }
-                }
-                //Zerar tudo
-                bottomPosition = 50; 
-                document.getElementsByClassName("char-damage")[0].style.display = "flex";
-                
-                setInterval(showEnemyDamage, 1); 
-
-                //Ex. Damage
-                enemySkills[Math.ceil(enemySkillNumber)][2];
-
-                //Def Extra
-                enemySkills[Math.ceil(enemySkillNumber)][3];
-
-                //Cura
-                enemySkills[Math.ceil(enemySkillNumber)][4];
-            }
-
-
-            //Mostrar Seta do personagem
-            document.getElementsByClassName("pixel-left-arrow")[0].style.display = "block"; 
-            //Tirar seta do inimigo
-            document.getElementsByClassName("pixel-right-arrow")[0].style.display = "none";
-
-            document.getElementsByClassName("mana-number")[1].innerHTML = enemyMana;
-            document.getElementsByClassName("mana-width")[1].style.width = enemyMana+"%";
-            
-            charTurn = true;
-
-            //Habilitar botões
-            for(let i = 0; i < 6; i++) {
-                document.getElementsByClassName("skill")[i].disabled = false;
-            }
-        }
-
-        setTimeout(enemyAttacks, 2000);
+        enemyTurn();
     }
     
 }
 
+function enemyTurn() {
+    document.getElementsByClassName("pixel-right-arrow")[0].style.display = "block";
+    document.getElementsByClassName("pixel-left-arrow")[0].style.display = "none";
+
+     //Adicionar 10 de mana
+     enemyMana+= 10;
+     if(enemyMana > 100){
+         enemyMana = 100;
+     }
+     document.getElementsByClassName("mana-width")[1].style.width = enemyMana+"%";
+     document.getElementsByClassName("mana-number")[1].innerHTML = enemyMana;
+    
+
+    function enemyAttacks() {
+
+        //Faz um calculo com a mana pra sortear uma skill que esteja dentro do limite da mana
+        enemySkillNumber = (enemyMana / 10) / 2;
+            
+        if(enemySkills[Math.ceil(enemySkillNumber)][0] > enemyMana) {
+            //Conta vai dar 6, que é a skill referente a mana zerada
+            enemySkillNumber = 120;
+            let teste = 0;
+
+            charLife = teste - charHeal; //Vida do Jogador;
+            charLifeHealAux = charLifeHealAux + charLife;
+            charLifeHealAuxTotal = 100 - charLifeHealAux;
+
+            document.getElementsByClassName("life-width")[0].style.width = charLifeHealAuxTotal+"%";
+            document.getElementsByClassName("life-number")[0].innerHTML = charLifeHealAuxTotal;
+
+        }else {
+            if(enemyMana >= 20) {
+                //Porcentagem da vida do Char
+                let teste = enemySkills[Math.ceil(enemySkillNumber)][1] - 
+                (enemySkills[Math.ceil(enemySkillNumber)][1] / 100 * 25);
+
+                charLife = teste - charHeal; //Vida do Jogador;
+                console.log(charLife);
+                charLifeHealAux = charLifeHealAux + charLife;
+                charLifeHealAuxTotal = 100 - charLifeHealAux;
+
+                document.getElementsByClassName("life-width")[0].style.width = charLifeHealAuxTotal+"%";
+                document.getElementsByClassName("life-number")[0].innerHTML = charLifeHealAuxTotal;
+            }
+                
+
+            //Tirar mana
+            enemyMana = enemyMana - enemySkills[Math.ceil(enemySkillNumber)][0];
+
+            //Aparecer dano no jogador
+            document.getElementsByClassName("char-damage")[0].innerHTML = "-" + 
+            enemySkills[Math.ceil(enemySkillNumber)][1];
+
+            //Mover ele até sumir
+            function showEnemyDamage(){
+                bottomPosition = bottomPosition + 0.1;
+                document.getElementsByClassName("char-damage")[0].style.bottom = bottomPosition + "%";
+
+                if(Math.round(bottomPosition) === 68) {
+                    document.getElementsByClassName("char-damage")[0].style.display = "none";
+                }
+            }
+            //Zerar tudo
+            bottomPosition = 50; 
+            document.getElementsByClassName("char-damage")[0].style.display = "flex";
+            
+            setInterval(showEnemyDamage, 1); 
+
+            //Ex. Damage
+            enemySkills[Math.ceil(enemySkillNumber)][2];
+
+            //Def Extra
+            enemySkills[Math.ceil(enemySkillNumber)][3];
+
+            //Cura
+            enemySkills[Math.ceil(enemySkillNumber)][4];
+        }
+
+
+        //Mostrar Seta do personagem
+        document.getElementsByClassName("pixel-left-arrow")[0].style.display = "block"; 
+        //Tirar seta do inimigo
+        document.getElementsByClassName("pixel-right-arrow")[0].style.display = "none";
+
+        document.getElementsByClassName("mana-number")[1].innerHTML = enemyMana;
+        document.getElementsByClassName("mana-width")[1].style.width = enemyMana+"%";
+        
+        charTurn = true;
+
+        //Adicionar mana para o jogador a cada round
+        charMana = charMana + 8;
+        if(charMana > 100){
+            charMana = 100;
+        }
+        document.getElementsByClassName("mana-width")[0].style.width = charMana+"%";
+        document.getElementsByClassName("mana-number")[0].innerHTML = charMana;
+
+        //Habilitar botões
+        for(let i = 0; i < 7; i++) {
+            document.getElementsByTagName("button")[i].disabled = false;
+        }
+    }
+
+    setTimeout(enemyAttacks, 2000);
+
+}
 
 if(charTurn) {
     //Mostrar Seta do personagem
     document.getElementsByClassName("pixel-left-arrow")[0].style.display = "block"; 
     //Tirar seta do inimigo
     document.getElementsByClassName("pixel-right-arrow")[0].style.display = "none";
+
+}
+
+function passTurn() {
+    charTurn = false;
+    enemyTurn();
+
+    //Desabilitar botão
+    document.getElementsByClassName("pass-turn")[0].disabled = true;
+    //Mostrar Seta do personagem
+    document.getElementsByClassName("pixel-left-arrow")[0].style.display = "none"; 
+    //Tirar seta do inimigo
+    document.getElementsByClassName("pixel-right-arrow")[0].style.display = "block";
 }
